596f6eb359e36ec0870c9084a170f25f52052ce4
Petros Pissias <8235623+ppissias@users.noreply.github.com>
2020-02-25 09:15:00 +0100

    minor code doc updates
    minor code doc updates

--- src/main/java/io/github/ppissias/pscanner/CommandExecutor.java
+++ src/main/java/io/github/ppissias/pscanner/CommandExecutor.java
@@ -5,19 +5,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * Simple class that executes a command and reads the first line of its output
+ * @author Petros Pissias
+ *
+ */
 public class CommandExecutor {
-  public static String getCommandOutput(String cmd) {
-    ProcessBuilder builder = new ProcessBuilder(new String[] { "bash", "-c", cmd });
-    builder.redirectErrorStream(true);
-    try {
-      Process process = builder.start();
-      InputStream is = process.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-      String line = reader.readLine();
-      process.destroyForcibly();
-      return line;
-    } catch (IOException e) {
-      return "cannot execute command";
-    } 
-  }
+
+	/**
+	 * Executes a command and returns its first line of output
+	 * @param cmd the command to be executed
+	 * @return first line of the command output
+	 */
+	public static String getCommandOutput(String cmd) {
+		ProcessBuilder builder = new ProcessBuilder(new String[] { "bash", "-c", cmd });
+		builder.redirectErrorStream(true);
+		try {
+			//exec command
+			Process process = builder.start();
+			
+			//get output and read line
+			InputStream is = process.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			String line = reader.readLine();
+			
+			//stop command execution if needed
+			process.destroyForcibly();
+			
+			//return first command output line
+			return line;
+			
+		} catch (IOException e) {
+			//don't care at this point to return an exception. Just return an error string
+			return "ERR:cannot execute command";
+		}
+	}
 }
--- src/main/java/io/github/ppissias/pscanner/Main.java
+++ src/main/java/io/github/ppissias/pscanner/Main.java
@@ -19,28 +19,37 @@ public class Main {
       System.out.println("file argument missing");
       return;
     } 
+    
+    //read provided the XML file
     DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
     DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
     Document document = docBuilder.parse(new File(args[0]));
     NodeList nodeList = document.getElementsByTagName("*");
+    
+    //to keep record of the comparison results
     StringBuffer packagesNotInstalled = new StringBuffer();
     StringBuffer packagesDiffVers = new StringBuffer();
     Integer totalNumberOfPackages = Integer.valueOf(0);
+    
+    //for each element in the XML file 
     for (int i = 0; i < nodeList.getLength(); i++) {
-      System.out.print("*");
-      Integer integer1 = totalNumberOfPackages, integer2 = totalNumberOfPackages = Integer.valueOf(totalNumberOfPackages.intValue() + 1);
+      System.out.print("*"); //report progress 
+      
       Node node = nodeList.item(i);
-      if (node.getNodeType() == 1) {
+      if (node.getNodeType() == 1) { //correct HTML elements 
         NamedNodeMap attributes = node.getAttributes();
+        
         if (attributes.getNamedItem("kind") != null && 
           attributes.getNamedItem("kind").getNodeValue().equals("package")) {
-          String name = attributes.getNamedItem("name").getNodeValue();
-          String ver = attributes.getNamedItem("ver").getNodeValue();
-          String rel = attributes.getNamedItem("rel").getNodeValue();
-          String pkgFullName = name + "-" + ver + "-" + rel;
-          String command = "rpm -q " + name;
-          String commandOutput = CommandExecutor.getCommandOutput(command);
-          if (commandOutput.startsWith(name)) {
+        	//found an entry we need to process, get details 
+        	String name = attributes.getNamedItem("name").getNodeValue();
+	        String ver = attributes.getNamedItem("ver").getNodeValue();
+	        	        
+	        //build command 
+	        String command = "rpm -q " + name;
+	        //execute command and get output 
+	        String commandOutput = CommandExecutor.getCommandOutput(command);
+          if (commandOutput.startsWith(name)) { //package installed 
             String packageVesion = commandOutput.substring(name.length() + 1);
             String[] versions = packageVesion.split("-");
             if (versions.length != 2) {
@@ -53,17 +62,24 @@ public class Main {
           } 
         } 
       } 
-    } 
+    } //finished processing CXML doc 
+    
+    //process results for presentation
     String[] pArray = packagesNotInstalled.toString().split("\n");
     Arrays.parallelSort(pArray);
     packagesNotInstalled = new StringBuffer();
-    for (int j = 0; j < pArray.length; j++)
-      packagesNotInstalled.append(pArray[j] + "\n"); 
+    for (int j = 0; j < pArray.length; j++) {
+      packagesNotInstalled.append(pArray[j] + "\n");
+    }
+    
     String[] vDiffArray = packagesDiffVers.toString().split("\n");
     Arrays.parallelSort(vDiffArray);
     packagesDiffVers = new StringBuffer();
-    for (int k = 0; k < vDiffArray.length; k++)
-      packagesDiffVers.append(vDiffArray[k] + "\n"); 
+    for (int k = 0; k < vDiffArray.length; k++) {
+      packagesDiffVers.append(vDiffArray[k] + "\n");
+    }
+    
+    //output
     System.out.println("");
     System.out.println("MISSING PACKAGES\n" + packagesNotInstalled);
     System.out.println("\nDIFFERENT VERSIONS\n" + packagesDiffVers);
